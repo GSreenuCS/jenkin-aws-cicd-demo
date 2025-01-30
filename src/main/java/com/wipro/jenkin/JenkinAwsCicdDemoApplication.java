8a068423aae7bcfef6ccf416fc86fe3982f90ef0
GSreenuCS <gsreenu.cs517@gmail.com>
2025-01-30 10:04:54 +0000

    first commit with jar file

---
+++ src/main/java/com/wipro/jenkin/JenkinAwsCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinAwsCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinAwsCicdDemoApplication.class, args);
+	}
+
+}
